29ade4b83e1ecc1f8548cda9f90035e9c6e34acd
mangix <maqh1988@gmail.com>
2015-11-30 19:34:11 +0800

    routers

--- examples/event.js
+++ examples/event.js
@@ -7,10 +7,9 @@ var Cat = require('../index');
  * E16:44:18.621    test-node-0 log-event-0     hello-world-0
  * E16:44:19.638    test-node-1 log-event-1 1   hello-world-1
  */
-for(var i=0;i<1000;i++) {
-    Cat.logEvent('category-0', 'category-1');
-}
+
 
 setTimeout(function () {
+    Cat.logEvent('category-0', 'category-1');
     Cat.logEvent('category-0-1', 'category-1-1', Cat.STATUS.FAIL, "some data");
 }, 1000);
